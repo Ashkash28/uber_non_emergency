8296a5e5885bef6b1645621a66e46637fa8f7c11
Gregg Hanano <gregg.hanano@gmail.com>
2015-05-30 02:24:41 -0700

    updated server.js and added passport module

--- server.js
+++ server.js
@@ -5,23 +5,33 @@ var app = express();
 
 var http = require('http');
 
+var passport = require('passport');
+
+var uberStrategy = require('passport-uber');
+
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
 
 app.use(express.static(path.join(__dirname, './client')));
+// app.use(passport.initialize());
+// app.use(passport.session());
+passport.use(new uberStrategy({
+    clientID: 'NvzG65eVVnZiVprITMGrUM8R3eqbqFky',
+    clientSecret: 'dWZGdg9kdY1aYNH0dMlKA4Q8QiEA0cqC4Tvgwt25',
+    callbackURL: "http://127.0.0.1:3000/auth/uber/callback"
+  },
+  function(accessToken, refreshToken, profile, done) {
+  	console.log("hi");
+    // User.find({ uberid: profile.id }, function (err, user) {
+    // 	console.log("profile id //////", profile.id);
+    //   return done(err, user);
+    // });
+  }
+));
 
-// app.get('/', function(req, res)
-// {
-// 	res.render('index');
-// })
-
-// app.get('/partial2', function(req, res)
-// {
-// 	res.render('view2');
-// })
-require('./config/mongoose');
-require('./config/routes')(app);
+// require('./config/mongoose');
+// require('./config/routes')(app);
 
 app.listen(8000, function()
 {
